df53c4e5f54b516cbae35ae66ff5bd00c96bfc13
Alex <alex.white017@gmail.com>
2026-05-08 16:50:42 -0500

    Make CORS allowed_origins env-driven for production

--- api/config/cors.php
+++ api/config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    'allowed_origins' => array_filter([
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        env('FRONTEND_URL'),
+    ]),
 
     'allowed_origins_patterns' => [],
 
